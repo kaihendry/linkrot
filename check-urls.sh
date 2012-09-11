4e53f216a9ede78d49db2785034b92e12b0dbd4c
Kai Hendry <hendry@webconverger.com>
2012-09-11 11:25:29 +0200

    initial

---
+++ check-urls.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+while read mdwn url
+do
+	set -- $(curl -m5 -w '%{http_code} %{url_effective}' -L -s -I --output /dev/null $url)
+	if test "$url" != "$2" # Is the original URL the same as the url_effective ?
+	then # if not, append the new URL on the line
+		echo -e "$1\t$mdwn\t${url}\t$2"
+	else
+		echo -e "$1\t$mdwn\t$url"
+	fi
+done
