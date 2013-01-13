8db1cdc7356fa3494e740f01d753fedbfe11e984
Kai Hendry <hendry@webconverger.com>
2013-01-13 14:31:21 +0800

    combining

--- check-urls.sh
+++ check-urls.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+# Can this be parallized some how?
+verbose=1
 
-while IFS=: read -r mdwn url
+while read -r file url
 do
-	set -- $(curl -m5 -w '%{http_code} %{url_effective}' -L -s -I --output /dev/null $url)
+	set -- $(curl -m5 -w '%{http_code} %{url_effective}' -L -s -I --output /dev/null "$url")
+	test "$verbose" || { test "$1" -eq "200" && continue; }
 	if test "$url" != "$2" # Is the original URL the same as the url_effective ?
 	then # if not, append the new URL on the line
-		echo -e "$1\t$mdwn\t${url}\t$2"
+		echo -e "$1\t$file\t${url}\t$2"
 	else
-		echo -e "$1\t$mdwn\t$url"
+		echo -e "$1\t$file\t$url"
 	fi
 done
